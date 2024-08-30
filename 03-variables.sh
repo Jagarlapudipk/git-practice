628e545721a598fe251cc3337994bc753d1f10f2
Pava <jpavan.kumar83@gmail.com>
2024-08-30 14:23:07 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+PERSON1=Pavan
+PERSON2=kumar
 
-
-echo "pavan:: Hi Kumar, How are you?"
-echo "kumar:: Hello pava. I am fine. How are you dong?"
-echo "pavan:: I am doing good kumar. what's going on?"
-echo "kumar:: I started learning shell scripting pavan"
+echo "$PERSON1:: Hi ${PERSON2}, How are you?"
+echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you dong?"
+echo "$PERSON1:: I am doing good ${PERSON2}. what's going on?"
+echo "${PERSON2}:: I started learning shell scripting $PERSON1"
