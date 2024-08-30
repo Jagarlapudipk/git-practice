f876e846cd6e23d14449cf71d03789146a78f5c9
Pava <jpavan.kumar83@gmail.com>
2024-08-30 10:04:42 +0530

    added conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "pavan:: Hi Kumar, How are you?"
+echo "kumar:: Hello pavan. I am fine. How are you doing"
+echo "Pavan:: I am doing good kumar. What's going on?"
+echo "kumar:: I started learning shell script pavan"
