97fa36b5a730f36582358efbbe9dc3825b7ec24a
Pava <jpavan.kumar83@gmail.com>
2024-08-30 14:00:02 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "pavan:: Hi Kumar, How are you?"
+echo "kumar:: Hello pava. I am fine. How are you dong?"
+echo "pavan:: I am doing good kumar. what's going on?"
+echo "kumar:: I started learning shell scripting pavan"
