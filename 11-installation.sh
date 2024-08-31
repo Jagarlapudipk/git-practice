f301d7a238faf5d66f296be582789bb6bee224e7
Pava <jpavan.kumar83@gmail.com>
2024-08-31 18:06:01 +0530

    Installation

---
+++ 11-installation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERID=$(id -u)
+echo "userid is: $USERID"
+
+if [ $USERID -ne 0 ]
+then    
+    echo "please run this script with root preveleges"
+    exit 1
+fi
+
+dnf install git -y
+
+
+
+
