1640d33ef708764e2398c8959a92a54854a2c289
Pava <jpavan.kumar83@gmail.com>
2024-09-01 16:19:57 +0530

    if else condition

--- 11-installation.sh
+++ 11-installation.sh
@@ -9,19 +9,40 @@ then
     exit 1
 fi
 
-dnf list install git 
+dnf list installed git 
 
 if [ $? -ne 0 ]
 then
     echo "Git is not installed, going to install it"
     dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not success"
+        exit 1
+    else
+        echo "Git installation is success"
+    fi
 
 elase
     echo "Git is already installed"
 
 fi
 
+dnf list installed mysql
 
-
+if [ $? -ne 0 ]
+then  
+    echo "MYSQL is not installed...going to install"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "MYSQL installation is failure..please check"
+        exit 1
+    else
+        echo "MYSQL installation is success"
+    fi
+else 
+    echo "MYSQL is already installed"
+fi
 
 
