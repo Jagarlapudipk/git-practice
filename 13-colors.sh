74180b0d959e018baa09969ba1a14e305d736bc4
Pava <jpavan.kumar83@gmail.com>
2024-09-01 17:51:16 +0530

    color

--- 13-colors.sh
+++ 13-colors.sh
@@ -21,3 +21,29 @@ VALIDATE(){
         echo -e "$2 is ...$G SUCCESS $N"
     fi
 }
+
+CHECK_ROOT
+
+dnf list installed git 
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it"
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+    
+    elase
+    echo "Git is already installed"
+
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then  
+    echo "MYSQL is not installed...going to install"
+    dnf install mysql -y
+    VALIDATE $? "Installing MYSQL"
+else 
+    echo "MYSQL is already installed"
+fi
